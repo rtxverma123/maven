e1961e844888d5fc5d5b506423df1638cffa5723
tamish.verma@ibm.com <tamish.verma@outlook.com>
2022-06-20 16:37:34 +0530

    Fixed exec

--- src/main/java/com/tam/vers/App.java
+++ src/main/java/com/tam/vers/App.java
@@ -1,5 +1,4 @@
 package com.tam.vers;
-import org.apache.commons.codec.digest.DigestUtils;
 /**
  * Hello world!
  *
@@ -8,15 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        if (args.length < 1) {
-            System.err.println("Please provide an input!");
-            System.exit(0);
-        }
-        System.out.println(sha256hex(args[0]));
+        System.out.println("Hi successfully ran the Java App");
 
-    }
-
-    public static String sha256hex(String input) {
-        return DigestUtils.sha256Hex(input);
     }
 }
